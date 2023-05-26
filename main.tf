86605beb737ecab9c09f474f302810d175e0ff5b
andrej988 <andrej.jagar@gmail.com>
2023-05-26 23:59:45 +0200

    Updated dependencies

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.0.0"
+      version = ">= 5.0.1"
     }
     archive = {
       source  = "hashicorp/archive"
