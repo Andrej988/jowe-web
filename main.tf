e46fa8bddc2b90f014058e2a9d550046859cfbab
andrej988 <andrej.jagar@gmail.com>
2023-06-25 11:47:14 +0200

    Updated workflows

--- main.tf
+++ main.tf
@@ -1,11 +1,5 @@
 terraform {
-  cloud {
-    organization = "initialised-si"
-
-    workspaces {
-      name = "JoWe-Web"
-    }
-  }
+  cloud {}
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -16,5 +10,5 @@ terraform {
       version = ">= 2.4.0"
     }
   }
-  required_version = ">= 1.0"
+  required_version = ">= 1.2.0"
 }
