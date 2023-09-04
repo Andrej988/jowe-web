41deaf20d0d2a9bdc202f48fba90704d50d9dcc9
andrej988 <andrej.jagar@gmail.com>
2023-09-04 23:34:25 +0200

    Added select boxes for ingredients and linked them together

--- src/components/utils/FormSelectGroupWithFeedbackEnhanced.tsx
+++ src/components/utils/FormSelectGroupWithFeedbackEnhanced.tsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
-import Select from 'react-select';
+import React from 'react';
+import Select, { ActionMeta, SingleValue } from 'react-select';
 import './FormSelectGroupWithFeedbackEnhanced.css';
 import { CFormLabel, CInputGroup, CInputGroupText } from '@coreui/react';
 import CIcon from '@coreui/icons-react';
 import FormElementFeedback from './FormElementFeedback';
 
-interface Option {
+export interface ListOption {
   value: string;
   label: string;
 }
@@ -14,20 +14,17 @@ interface Props {
   className?: string | undefined;
   icon: string | string[];
   id: string;
+  value: ListOption | null;
+  onChange?: (newValue: SingleValue<ListOption>, actionMeta: ActionMeta<ListOption>) => void;
   label?: string;
   placeholder: string;
-  options: Option[];
+  options: ListOption[];
   feedbackMsg?: string;
   showFeedbackMsg?: boolean;
+  disabled?: boolean;
 }
 
 const FormSelectGroupWithFeedbackEnhanced: React.FC<Props> = (props) => {
-  const [selectedOption, setSelectedOption] = useState<Option | null>(null);
-
-  const handleChange = (option: Option | null) => {
-    setSelectedOption(option);
-  };
-
   return (
     <div className={props.className}>
       {props.label && <CFormLabel htmlFor={props.id}>{props.label}</CFormLabel>}
@@ -38,14 +35,15 @@ const FormSelectGroupWithFeedbackEnhanced: React.FC<Props> = (props) => {
         <Select
           id={props.id}
           options={props.options}
-          value={selectedOption}
-          onChange={handleChange}
+          value={props.value}
+          onChange={props.onChange}
           className="react-select-container"
           classNamePrefix="react-select"
           placeholder={props.placeholder}
+          isDisabled={props.disabled || props.options.length === 0}
         />
       </CInputGroup>
-      {props.feedbackMsg != null && !selectedOption && props.showFeedbackMsg === true && (
+      {props.feedbackMsg != null && !props.value && props.showFeedbackMsg === true && (
         <FormElementFeedback feedbackMsg={props.feedbackMsg} />
       )}
     </div>
